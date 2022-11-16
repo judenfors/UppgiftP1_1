ee19c76863d02249081d4cba662051b2560145b2
Admin <Admin@TEB123022>
2022-11-16 12:00:53 +0100

    Add project files.

---
+++ UppgiftP1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UppgiftP1_1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hej och välkommen till lyckohjulet, vänligen skriv in ett tal mellan 1-25");
+            int inmatatTal=int.Parse(Console.ReadLine());
+
+            Random random=new Random();
+            int hjuletsnummer=random.Next(1, 26);
+
+            Console.WriteLine($"Lyckohjulet stannade på {hjuletsnummer}");
+
+            if (inmatatTal == hjuletsnummer)
+            {
+                Console.WriteLine("Du vann lyckohjulet, grattis!");
+            }
+
+            else
+            {
+                Console.WriteLine("Du förlorade tyvärr");
+            }
+
+
+        }
+    }
+}
